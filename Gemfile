3b7f83cb61fb017e890d1447ded013aafe7f5dd6
Freddie Fujiwara <github@ze.gs>
2012-08-18 11:17:46 +0900

    capify

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :rubygems
+source "http://rubygems.org"
+group :deployment do
+	  gem 'rvm-capistrano'
+	  gem 'capistrano'
+	  gem 'capistrano-ext'
+	  gem 'capistrano_colors'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+require 'rvm/capistrano'
+set :application, "VendingMachine Test"
+set :repository,  "git://github.com/freddiefujiwara/tddbcsendai02-feedback-ui.git"
+
+set :scm, :git
+role :server, 'localhost'
+set :deploy_to ,"/usr/share/nginx/www/tddbcsendai02.ze.gs/"
+namespace :deploy do
+  task :setup do
+      run "source ~/.nvm/nvm.sh;nvm use v0.8.6;cd #{current_path}; make"
+  end
+end
+after "deploy", "deploy:setup"

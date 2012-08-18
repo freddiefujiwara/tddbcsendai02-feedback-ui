f4abfa241c18d276dbbc932280b217bb42e4e317
Freddie Fujiwara <github@ze.gs>
2012-08-18 11:24:59 +0900

    use -> fumikazu

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :repository,  "git://github.com/freddiefujiwara/tddbcsendai02-feedback-ui.gi
 set :scm, :git
 role :server, 'localhost'
 set :deploy_to ,"/usr/share/nginx/www/tddbcsendai02.ze.gs/"
+set :user, "fumikazu"
 namespace :deploy do
   task :setup do
       run "source ~/.nvm/nvm.sh;nvm use v0.8.6;cd #{current_path}; make"
